379cb15961d93fba910e926dc94894eca27c72e7
sala7khaled <sala7khaled7@gmail.com>
2024-10-02 19:18:48 +0300

    init files

---
+++ BM Movies/SplashController.swift
@@ -0,0 +1,29 @@
+//
+//  SplashController.swift
+//  BM Movies
+//
+//  Created by Salah Khaled on 02/10/2024.
+//
+
+import UIKit
+
+class SplashController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
